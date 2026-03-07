2ca46440543a8cc4cf175466d2b64c6d79cce746
FamALouiz <fam@awadlouis.com>
2026-03-07 12:17:20 +0100

    feat: add utility function for pair tensor contraction

---
+++ src/tensor_network/utils/contraction.py
@@ -0,0 +1,38 @@
+"""Tensor network contraction utilities."""
+
+from copy import deepcopy
+
+from tensor_network import TensorNetwork
+
+
+def contract_pair(network: TensorNetwork, pair: tuple[int, int]) -> TensorNetwork:
+    """Contract a pair of tensors in the network.
+
+    Args:
+        network: The tensor network to contract.
+        pair: A tuple of two integers representing the indices of the tensors to contract.
+
+    Returns:
+        The resulting tensor network after contraction.
+    """
+    assert network.num_tensors > pair[0] and network.num_tensors > pair[1], (
+        "Tensor indices out of range."
+    )
+
+    new_tensor_network = deepcopy(network)
+
+    contracted_indices = set(network.input_indices[pair[0]]) & set(network.input_indices[pair[1]])
+    new_tensor_indices = (
+        set(network.input_indices[pair[0]]) | set(network.input_indices[pair[1]])
+    ) - contracted_indices
+    new_tensor_shape = tuple(network.size_dict[index] for index in new_tensor_indices)
+
+    new_tensor_network.input_indices.insert(pair[0], list(new_tensor_indices))
+    new_tensor_network.shapes.insert(pair[0], new_tensor_shape)
+
+    new_tensor_network.input_indices.remove(network.input_indices[pair[0]])
+    new_tensor_network.input_indices.remove(network.input_indices[pair[1]])
+    new_tensor_network.shapes.remove(network.shapes[pair[0]])
+    new_tensor_network.shapes.remove(network.shapes[pair[1]])
+
+    return new_tensor_network
