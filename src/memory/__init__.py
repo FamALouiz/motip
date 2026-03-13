44ff2f29554033cebfcb2d41f91e4dbbf92f4cc0
FamALouiz <fam@awadlouis.com>
2026-03-13 21:42:28 +0100

    refactor: remove lazy import

--- src/memory/__init__.py
+++ src/memory/__init__.py
@@ -1,16 +1,5 @@
 """Memory module for the motip package."""
 
-from importlib import import_module
-
 from memory.memory import Memory, MemorySizes
 
 __all__ = ["Memory", "MemorySizes", "utils", "calculator"]
-
-
-def __getattr__(name: str):
-    """Lazily import submodules."""
-    if name == "utils":
-        return import_module("memory.utils")
-    if name == "calculator":
-        return import_module("memory.calculator")
-    raise AttributeError(f"module 'memory' has no attribute '{name}'")
--- src/tensor_network/__init__.py
+++ src/tensor_network/__init__.py
@@ -1,14 +1,5 @@
 """Tensor network module."""
 
-from importlib import import_module
-
 from tensor_network.tn import ContractionPath, TensorNetwork
 
 __all__ = ["TensorNetwork", "ContractionPath", "builder"]
-
-
-def __getattr__(name: str):
-    """Lazily import submodules."""
-    if name == "builder":
-        return import_module("tensor_network.builder")
-    raise AttributeError(f"module 'tensor_network' has no attribute '{name}'")
