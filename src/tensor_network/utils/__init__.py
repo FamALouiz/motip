9edb79e25b5dcc4e74655b64aacccba1042bfd11
FamALouiz <fam@awadlouis.com>
2026-03-15 18:40:25 +0100

    feat: add permute tensor utility

--- src/tensor_network/utils/__init__.py
+++ src/tensor_network/utils/__init__.py
@@ -1,5 +1,5 @@
 """Utils module for the motip package."""
 
-from tensor_network.utils import contraction, random
+from tensor_network.utils import contraction, permutation, random
 
-__all__ = ["random", "contraction"]
+__all__ = ["random", "contraction", "permutation"]
