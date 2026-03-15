19a73f69bb0aa82d13a5669927db688ea43a291f
FamALouiz <fam@awadlouis.com>
2026-03-15 15:54:02 +0100

    feat: add largest intermediate tensor utility function

--- src/memory/utils.py
+++ src/memory/utils.py
@@ -1,8 +1,11 @@
 """Utility functions for memory operations."""
 
+from copy import deepcopy
+
 from memory.calculator import MemoryCalculator
 from memory.memory import Memory, MemorySizes
-from tensor_network import TensorNetwork
+from tensor_network import ContractionPath, TensorNetwork
+from tensor_network.utils.contraction import contract_pair
 
 
 def get_memory_from_string(memory_str: str) -> Memory:
@@ -47,3 +50,37 @@ def get_largest_tensor_in_network(network: TensorNetwork) -> tuple[int, Memory]:
             largest_memory = tensor_memory
             largest_tensor_idx = i
     return largest_tensor_idx, largest_memory
+
+
+def get_largest_intermediate_tensor_in_contraction_path(
+    network: TensorNetwork, path: ContractionPath
+) -> tuple[int, Memory]:
+    """Get the memory requirements of the largest intermediate tensor in a contraction path.
+
+    This is the largest intermediate tensor that is created during the contraction process, which
+    may be any of the original tensors in the network.
+
+    Args:
+        network: The tensor network to analyze.
+        path: A list of pairs of tensor indices that are contracted together.
+
+    Returns:
+        A tuple containing the index of the contraction step in the path that created the largest
+        intermediate tensor (AFTER performing the contraction at the given index) and its memory
+        requirements. If the index is -1, it indicates that the largest tensor is one of the
+        original tensors in the network.
+    """
+    _, largest_memory = get_largest_tensor_in_network(network)
+    largest_contraction_step_idx = -1  # -1 indicates largest tensor is from the original network
+    intermediate_network = deepcopy(network)
+
+    for contraction_idx, contraction_pair in enumerate(path):
+        intermediate_network = contract_pair(intermediate_network, contraction_pair)
+        intermediate_tensor = intermediate_network.tensors[contraction_pair[0]]
+        intermediate_memory = MemoryCalculator().calculate_memory_for_tensor(intermediate_tensor)
+
+        if intermediate_memory > largest_memory:
+            largest_memory = intermediate_memory
+            largest_contraction_step_idx = contraction_idx
+
+    return largest_contraction_step_idx, largest_memory
