0315724494b14bb12e02b822f7c171d032151b4a
FamALouiz <fam@awadlouis.com>
2026-05-19 13:48:50 +0200

    test: fix test_tensor_operation_result_from_tensor_creates_result_with_given_tensor

--- test/operations/test_utils.py
+++ test/operations/test_utils.py
@@ -2,6 +2,7 @@
 
 from operations.base import TensorOperationResult
 from operations.utils import tensor_operation_result_from_tensor
+from tensor import Tensor
 
 
 class TestTensorOperationResultFromTensor:
@@ -9,7 +10,7 @@ class TestTensorOperationResultFromTensor:
 
     def test_tensor_operation_result_from_tensor_creates_result_with_given_tensor(self) -> None:
         """Test that the function creates a TensorOperationResult with the given tensor."""
-        tensor = "dummy_tensor"
+        tensor = Tensor(input_indices=[0, 1], shape=(2, 4), array=None)
         result = tensor_operation_result_from_tensor(tensor)
         assert isinstance(result, TensorOperationResult)
         assert result.tensor == tensor
